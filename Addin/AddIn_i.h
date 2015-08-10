eb2eb470829c9c4d47d4ce93755ce1e28e473022
nbelyh <nbelyh@gmail.com>
2015-08-10 13:47:07 +0200

    move to developer tab

--- Addin/AddIn_i.h
+++ Addin/AddIn_i.h
@@ -4,10 +4,10 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Wed Aug 05 14:40:15 2015
+/* at Thu Aug 06 11:40:33 2015
  */
 /* Compiler settings for AddIn.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
